93761b0b3b45f4bbee004c057111d3b665f4b03c
zhuan <z07pig@163.com>
2018-10-29 11:20:17 +0800

    添加日志配置文件

--- src/main/java/com/baidu/ai/MainApplication.java
+++ src/main/java/com/baidu/ai/MainApplication.java
@@ -2,8 +2,14 @@ package com.baidu.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @Auther: zhuan
+ * @Date: 2018/10/29 10:50
+ * @Description:
+ */
 
-@SpringBootApplication
+//自定义需要扫描的包，默认扫描到入口类所在的包
+@SpringBootApplication(scanBasePackages="com.baidu.ai")
 public class MainApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/baidu/ai/controller/TextDiscernController.java
+++ src/main/java/com/baidu/ai/controller/TextDiscernController.java
@@ -3,16 +3,32 @@ package com.baidu.ai.controller;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
+
+/**
+ * @Auther: zhuan
+ * @Date: 2018/10/29 10:50
+ * @Description:
+ */
+
 //@RestController注解能够使项目支持Rest
 @RestController
 @SpringBootApplication
 //表示该controller类下所有的方法都公用的一级上下文根
 @RequestMapping(value = "/TextDiscern")
 
-
 public class TextDiscernController {
 	@RequestMapping("/hello")
     public String index() {
+    /**
+     *
+     * 功能描述: 
+     *
+     * @param: []
+     * @return: java.lang.String
+     * @auther: zhuan
+     * @date: 2018/10/29 10:50
+     */
+	    
         return "Hello World";
     }
 }
